5a0ea5df7292fcfccc47fda93cd1ecf5d86df801
louohr <louise.ohrner@gmail.com>
2024-08-29 18:39:23 +0200

    add comments

--- src/Components/CountdownTimer.tsx
+++ src/Components/CountdownTimer.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 const CountdownTimer = () => {
   // State declarations
   const [timeLeft, setTimeLeft] = useState(5); // tid kvar
-  const [isActive, setIsActive] = useState(false); // pågående eller pausad, true/false
+  const [isActive, setIsActive] = useState(false); // uppdaterar timeleft, aktiv eller inte
   const timerRef = useRef(0); // reference to the interval timer, paus eller återställning
 
   useEffect(() => {
@@ -13,6 +13,7 @@ const CountdownTimer = () => {
       timerRef.current = setInterval(() => {
         setTimeLeft((prevTime) => {
           if (prevTime <= 1) {
+            // stannar timern
             clearInterval(timerRef.current);
             setIsActive(false);
             console.log("Tiden är slut!");
@@ -23,14 +24,14 @@ const CountdownTimer = () => {
       }, 1000);
     }
 
-    // start och stopp med cleanup
+    // stannar timer med cleanup
     return () => clearInterval(timerRef.current);
   }, [isActive, timeLeft]);
 
   // återställ timer
   const resetTimer = () => {
-    setIsActive(false);
-    clearInterval(timerRef.current);
+    setIsActive(false); // stoppar timer
+    clearInterval(timerRef.current); // rensar nuvarande värde för att stoppa timern
     setTimeLeft(5);
   };
 
